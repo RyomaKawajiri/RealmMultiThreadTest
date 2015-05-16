e7f8802c884d0b21f1e2e36f5257e8aca5be837d
Kawajiri Ryoma <ryoma.edison@gmail.com>
2015-05-16 13:19:04 +0900

    Initial Commit

---
+++ RealmMultiTheadTests/RealmMultiTheadTests.h
@@ -0,0 +1,19 @@
+//
+//  RealmMultiTheadTests.h
+//  RealmMultiTheadTests
+//
+//  Created by Kawajiri Ryoma on 5/16/15.
+//  Copyright (c) 2015 strobo inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmMultiTheadTests.
+FOUNDATION_EXPORT double RealmMultiTheadTestsVersionNumber;
+
+//! Project version string for RealmMultiTheadTests.
+FOUNDATION_EXPORT const unsigned char RealmMultiTheadTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmMultiTheadTests/PublicHeader.h>
+
+
