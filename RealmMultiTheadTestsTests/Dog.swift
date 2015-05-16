b829d9d627e750fe92f92b80dc7b50f1e6500f91
Kawajiri Ryoma <ryoma.edison@gmail.com>
2015-05-16 14:00:59 +0900

    Create sample realm object "Dog"

---
+++ RealmMultiTheadTestsTests/Dog.swift
@@ -0,0 +1,14 @@
+//
+//  Dog.swift
+//  RealmMultiTheadTests
+//
+//  Created by Kawajiri Ryoma on 5/16/15.
+//  Copyright (c) 2015 strobo inc. All rights reserved.
+//
+
+import RealmSwift
+
+// Dog model
+class Dog: Object {
+  dynamic var name = ""
+}
